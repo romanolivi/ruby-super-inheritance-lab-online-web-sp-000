69a208c5a1d93ff002051de3e94f567f5f57623f
Romano Livi <romano_livi@outlook.com>
2020-12-22 20:21:37 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,10 +1,10 @@
 class Student 
   
   def hello 
-    "Hey there! I'm so excited to learn stuff."
+    return "Hey there! I'm so excited to learn stuff."
   end
   
   def raise_hand 
-    "Pick me!"
+    return "Pick me!"
   end
 end
