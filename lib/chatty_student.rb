bb100357d6e416c7935422c34a218668382fc7e8
Romano Livi <romano_livi@outlook.com>
2020-12-22 20:25:44 +0000

    Automatically backed up by Learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -7,24 +7,6 @@ class ChattyStudent < Student
   
   def raise_hand
     super
-    puts "Pick me!"
-     super
-    puts "Pick me!"
-     super
-    puts "Pick me!"
-     super
-    puts "Pick me!"
-     super
-    puts "Pick me!"
-     super
-    puts "Pick me!"
-     super
-    puts "Pick me!"
-     super
-    puts "Pick me!"
-     super
-    puts "Pick me!"
-     super
-    puts "Pick me!"
+    puts ("Pick me!").times.ten
   end
 end
